a246605abf935af8a59785dab52e58f582fc0c60
Abhishekpandey008 <67016292+Abhishekpandey008@users.noreply.github.com>
2020-09-18 23:01:50 +0530

    Add files via upload

---
+++ task2tsf.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+url = "http://bit.ly/w-data"
+s_data = pd.read_csv(url)
+print("Data imported successfully")
+
+s_data.head()
+print(s_data)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+X = s_data.iloc[:, :-1].values #get a copy of dataset exclude last column
+y = s_data.iloc[:, 1].values #get array of dataset in column 1st
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+print("Training complete.")
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Visualizing the Training set results
+viz_train = plt
+viz_train.scatter(X_train, y_train, color='red')
+viz_train.plot(X_train, regressor.predict(X_train), color='blue')
+viz_train.title('Hours vs scores (Training set)')
+viz_train.xlabel('Hours')
+viz_train.ylabel('scores')
+viz_train.show()
+
+# Visualizing the Test set results
+viz_test = plt
+viz_test.scatter(X_test, y_test, color='red')
+viz_test.plot(X_train, regressor.predict(X_train), color='blue')
+viz_test.title('Hours vs scores (Test set)')
+viz_test.xlabel('Hours')
+viz_test.ylabel('scores')
+viz_test.show()
+
+# Comparing Actual vs Predicted
+df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+df
+
+#Evaluating the model
+from sklearn import metrics
+print('Mean Absolute Error:',
+      metrics.mean_absolute_error(y_test, y_pred))
+
+
+#testing own data
+hours = [[9.25]]
+own_pred = regressor.predict(hours)
+print("No of Hours = {}".format(hours))
+print("Predicted Score = {}".format(own_pred[0]))
+
+#Evaluating the model
+from sklearn import metrics
+print('Mean Absolute Error:',
+      metrics.mean_absolute_error(y_test, y_pred))
